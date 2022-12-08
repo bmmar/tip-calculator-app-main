eccde0cc788b4f445cd772f2e9c627847da0ec4f
bmmar <71896547+bmmar@users.noreply.github.com>
2022-12-08 14:53:08 +0000

    Mobile fully optnl.  About to start desktop

--- tip.js
+++ tip.js
@@ -1,4 +1,5 @@
 const billAmount = document.querySelector("#currency-field");
+const billEntryField = document.querySelector(".bill-amount");
 const fivePerCent = document.querySelector(".five-pc");
 const tenPerCent = document.querySelector(".ten-pc");
 const fifteenPerCent = document.querySelector(".fifteen-pc");
@@ -10,6 +11,10 @@ const numPeople = document.querySelector(".num-people");
 const tip = document.querySelector(".tip-per-person-amount");
 const totalPerPerson = document.querySelector(".total-per-person-amount");
 const resetBtn = document.querySelector(".reset-button");
+const billErrorMsg = document.querySelector("span.bill-error-msg");
+const peopleErrorMsg = document.querySelector(".people-error-msg");
+const numPeopleEntryField = document.querySelector(".num-people-input");
+const customPCEntry = document.querySelector("#custom-percent");
 
 const formatter = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 2,
@@ -20,11 +25,14 @@ let percent = 0;
 let tipAmount = 0;
 let total = 0;
 
-// Working.  Now some details to cover.  Need to validate $ amount (leading zeroes, 5 decimal places etc.)
-// Need to validate num of people entry with a  "cannot be zero" routine.
+// Working.  Now some details to cover.
 // The reset button should perhaps "indent" when clicked
 // Develop a routine for the custom %age button
-// And any valid combination of entries should display the calculated amounts.
+
+billAmount.addEventListener("blur", () => {
+  console.log(billAmount.value);
+  billAmount.value = parseFloat(billAmount.value);
+});
 
 fivePerCent.addEventListener("click", () => {
   clearBtnSelection();
@@ -58,8 +66,15 @@ fiftyPerCent.addEventListener("click", () => {
 });
 customPerCent.addEventListener("click", () => {
   clearBtnSelection();
-  customPerCent.classList.add("percent-clicked");
-  percent = 0;
+  customPCEntry.classList.add("custom-percent-clicked");
+  percent = parseInt(customPCEntry.value);
+  console.log("custom percent is: " + percent);
+  calculate();
+});
+
+customPCEntry.addEventListener("input", () => {
+  percent = parseInt(customPCEntry.value);
+  console.log("custom percent is: " + percent);
   calculate();
 });
 
@@ -70,7 +85,9 @@ numPeople.addEventListener("input", () => {
 resetBtn.addEventListener("click", () => {
   clearBtnSelection();
   billAmount.value = "0.00";
-  numPeople.value = "1";
+  numPeople.value = "0";
+  tip.textContent = "0.00";
+  totalPerPerson.textContent = "0.00";
 });
 
 function clearBtnSelection() {
@@ -78,35 +95,53 @@ function clearBtnSelection() {
     if (allPercentBtns[i].classList.contains("percent-clicked")) {
       allPercentBtns[i].classList.remove("percent-clicked");
     }
+    if (customPCEntry.classList.contains("custom-percent-clicked")) {
+      customPCEntry.classList.remove("custom-percent-clicked");
+    }
   }
 }
 
 function calculate() {
-  tipAmount = billAmount.value * (percent / 100);
+  if (percent === 0 || isNaN(percent)) {
+    tipAmount = 0;
+  } else {
+    tipAmount = billAmount.value * (percent / 100);
+  }  
   bill = parseFloat(billAmount.value);
   total = bill + tipAmount;
+
   if (validateAll()) {
     tip.textContent = "$" + formatter.format(tipAmount / numPeople.value);
     totalPerPerson.textContent =
       "$" + formatter.format(total / numPeople.value);
-    // console.log("Bill is for " + bill);
-    // console.log("Tip at " + percent + "% is " + formatter.format(tipAmount));
-    // console.log("Number of people: " + numPeople.value);
-    // console.log(
-    //   "Tip per person: " + formatter.format(tipAmount / numPeople.value)
-    // );
-    // console.log("Total per person: " + formatter.format(total / numPeople.value));
   }
 }
 
 function validateAll() {
-  // if (bill > 50000) {
-  //   console.log("Bill cannot be greater than $50000");
-  //    return false;
-  // }
-  if (numPeople.value === 0 || numPeople.value === "") {
-    console.log("Num people cannot be 0");
+  if (bill > 50000) {
+    billEntryField.classList.add("bill-amount-error");
+    billErrorMsg.textContent = "Can't be > $50000";
+    billErrorMsg.classList.add("active");
     return false;
+
+  } else if (bill === 0 || isNaN(bill)) { 
+    billEntryField.classList.add("bill-amount-error");
+    billErrorMsg.textContent = "Can't be 0";
+    billErrorMsg.classList.add("active");
+    return false;
+  }
+  else if (billErrorMsg.classList.contains("active")) {
+    billErrorMsg.classList.remove("active");
+    billEntryField.classList.remove("bill-amount-error");
+  }
+  if (numPeople.value === 0 || numPeople.value === "") {
+    console.log("People cannot be zero");
+    peopleErrorMsg.classList.add("active");
+    numPeopleEntryField.classList.add("num-people-error");
+    return false;
+  } else if (peopleErrorMsg.classList.contains("active")) {
+    peopleErrorMsg.classList.remove("active");
+    numPeopleEntryField.classList.remove("num-people-error");
   }
   return true;
 }
